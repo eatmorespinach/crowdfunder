6e7ef6ccf1039b9d2dae2a810068e07c13b40d71
Drew Sing <sing.drew@gmail.com>
2013-07-18 13:40:30 -0400

    REFUTE is important in testing for rails 4. Tests now pass

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -2,5 +2,5 @@ class Pledge < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
   validates :user_id, :project_id, presence: true
-  validates :amount, presence: true, numericality: {only_integer: true}
+  validates :amount, numericality: {greater_than: 0, only_integer: true}, presence: true
 end
--- test/models/pledge_test.rb
+++ test/models/pledge_test.rb
@@ -5,25 +5,22 @@ class PledgeTest < ActiveSupport::TestCase
   # VALIDATIONS
   test "requires a user" do 
     pledge = FactoryGirl.build(:pledge, user: nil)
-    
     # Should not be able to save
     refute pledge.save
-    assert_equal "can't be blank", pledge.errors[:user].first
+    assert_equal "can't be blank", pledge.errors[:user_id].first
   end
   
   test "requires a project" do 
     pledge = FactoryGirl.build(:pledge, project: nil)
-
     # Should not be able to save
     refute pledge.save
-    assert_equal "can't be blank", pledge.errors[:project].first
+    assert_equal "can't be blank", pledge.errors[:project_id].first
   end
 
   test "requires amount" do 
     pledge = FactoryGirl.build(:pledge, amount: nil)
-
     # Should not be able to save
-    assert_false pledge.save
+    refute pledge.save
     assert_equal "is not a number", pledge.errors[:amount].first
   end
 
@@ -31,7 +28,7 @@ class PledgeTest < ActiveSupport::TestCase
     pledge = FactoryGirl.build(:pledge, amount: -10)
 
     # Should not be able to save
-    assert_false pledge.save
+    refute pledge.save
     assert_equal "must be greater than 0", pledge.errors[:amount].first
   end
   
@@ -39,7 +36,7 @@ class PledgeTest < ActiveSupport::TestCase
     pledge = FactoryGirl.build(:pledge, amount: 'abc')
 
     # Should not be able to save
-    assert_false pledge.save
+    refute pledge.save
     assert_equal "is not a number", pledge.errors[:amount].first
   end
 end
