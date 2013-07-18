d5ddcd601908990700a3d1d6b178bd8def8f78af
Drew Sing <sing.drew@gmail.com>
2013-07-17 20:18:14 -0400

    something's funny with these tests...

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -1,4 +1,6 @@
 class Pledge < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
+  validates :user_id, :project_id, presence: true
+  validates :amount, presence: true, numericality: {only_integer: true}
 end
